5c866b41b019b0bba88c6107fa9006412460227f
Ryan Mecca <rm358@duke.edu>
2020-02-23 19:01:57 -0500

    Edited front end view code.
    Added the turtle and animating the turtle.

--- src/slogo/view/SubSceneLeft.java
+++ src/slogo/view/SubSceneLeft.java
@@ -1,5 +1,6 @@
 package slogo.view;
 
+import javafx.animation.TranslateTransition;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.image.Image;
@@ -7,6 +8,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class SubSceneLeft extends SubScene {
 
@@ -19,10 +21,22 @@ public class SubSceneLeft extends SubScene {
 //    vBox.getStyleClass().add("leftvbox");
 //    root.getChildren().add(vBox);
     createRectangle();
-    createTurtle();
+    root.getChildren().add(createTurtle());
+    moveTurtle();
   }
 
-  private void createTurtle() {
+  private void moveTurtle() {
+    TranslateTransition trans = new TranslateTransition(Duration.seconds(2), turtle);
+    trans.setToY(-100);
+    turtle.setRotate(-45);
+    trans.setToX(-100);
+    trans.play();
+  }
+
+  private ImageView createTurtle() {
+    turtle.setX(200);
+    turtle.setY(150);
+    return turtle;
   }
 
   private void createRectangle() {
